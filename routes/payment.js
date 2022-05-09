6834f182830379624e94dc97a66205822f15ed87
mkumar0802 <mkumar0802@gmail.com>
2022-05-09 12:42:38 +0530

    make it better

--- routes/payment.js
+++ routes/payment.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router=express.Router();
 
-const paymentModule = require('../Module/PaymentModule')
+const paymentModule = require('../Module/paymentModule')
 
 router.post("/orders",paymentModule.orders );
 
